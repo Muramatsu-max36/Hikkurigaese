11437303a336ac70ec7b3a6d750d81906f753b62
Muramatsu Yuichi <21cm0136@jec.ac.jp>
2022-11-29 14:27:23 +0900

    commit

---
+++ Pods/Target Support Files/Pods-SampleApp0001-SampleApp0001UITests/Pods-SampleApp0001-SampleApp0001UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleApp0001_SampleApp0001UITests : NSObject
+@end
+@implementation PodsDummy_Pods_SampleApp0001_SampleApp0001UITests
+@end
